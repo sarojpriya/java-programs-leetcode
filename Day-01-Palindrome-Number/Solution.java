e50cdb261aee2c19ab1a4c1b32e165844cef4c36
saroj priya <sarojpriya0803@gmail.com>
2026-05-07 19:50:39 +0530

    Create Solution.java
    Solved: LeetCode Palindrome Number using Java.
    
    Concepts used:
    
    While loop
    Modulo and division
    Reversing an integer
    
    Approach:
    
    Reversed the given number digit by digit
    Compared the reversed value with the original number
    
    Learned:
    
    Negative numbers and numbers ending in 0 need special attention in palindrome problems.
    
    Time Complexity: O(log n)
    Space Complexity: O(1)
    
    #leetcode #java #dsa #programming #coding

---
+++ Day-01-Palindrome-Number/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public boolean isPalindrome(int x) {
+
+        if (x < 0) {
+            return false;
+        }
+
+        int original = x;
+        int sum = 0;
+
+        while (x != 0) {
+
+            int d = x % 10;
+            sum = sum * 10 + d;
+            x = x / 10;
+        }
+
+        return sum == original;
+    }
+}
